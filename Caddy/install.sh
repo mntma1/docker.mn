604631d3cbefdb650bda89a076162b319cfa8d07
Manfred N <114916491+mntma1@users.noreply.github.com>
2025-07-05 08:30:30 +0200

    Update install.sh by Manfred

--- Caddy/install.sh
+++ Caddy/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 docker run -d --cap-add=NET_ADMIN -p 80:80 -p 443:443 -p 443:443/udp \
     --name Caddy \
-    -v /data/site:/srv \
-    -v /data:/data \
+    -v /data/caddy/site:/srv \
+    -v /data/caddy/data:/data \
     -v caddy_config:/config \
     caddy caddy file-server --domain files.tma1.duckdns.org
